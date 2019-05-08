b4aa3c31c3aec387da477a3cee423383888ce07d
sizigia <cumbresdigitales@gmail.com>
2019-05-08 16:55:31 -0300

    solution to calculator using reduce

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,23 @@
-#write your code here
+def add(*numbers)
+  numbers.reduce(:+)
+end
+
+def subtract(*numbers)
+  numbers.reduce(:-)
+end
+
+def sum(array)
+  array.reduce(0, :+)
+end
+
+def multiply(*numbers)
+  numbers.reduce(:*)  
+end
+
+def power(*numbers)
+  numbers.reduce(:**)
+end
+
+def factorial(number)
+  (1..number).reduce(1, :*)
+end
