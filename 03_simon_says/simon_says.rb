bf813a8d92862f49d650d1edfee0faac9cf093b3
sizigia <cumbresdigitales@gmail.com>
2019-05-08 21:41:44 -0300

    solution to simon_says.rb

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,35 @@
-#write your code here
+def echo(phrase)
+  phrase
+end
+
+def shout(phrase)
+  phrase.upcase
+end
+
+def repeat(phrase, number = 2)
+  [phrase] * number * ' '
+end
+
+def start_of_word(word, letters)
+  word[0..(letters - 1)]
+end
+
+def first_word(phrase)
+  phrase.split[0]
+end
+
+def titleize(title)
+  def little_word(x)
+    ["and", "the", "of", "it", "over"].include?(x)
+  end
+
+  words = title.split
+  words.length.times do |i|
+    (little_word(words[0]))?
+      words[0].capitalize! :
+    (little_word(words[i]))?
+      words[i].downcase :
+      words[i].capitalize!
+    end
+  words.join(' ')
+end
