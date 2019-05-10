55635c32ea5380523cea6526052cb6fd461aeef3
sizigia <cumbresdigitales@gmail.com>
2019-05-09 21:55:22 -0300

    solution to book titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,16 @@
 class Book
-# write your code here
+  attr_accessor :title
+
+  def title
+    @title
+    def little_word(word)
+      ["and", "the", "of", "it", "over", "in", "an", "a"].include?(word)
+    end
+    @title.capitalize.split(' ').map {|i| 
+      if (little_word(i))
+        i.downcase
+      else
+        i.capitalize
+      end}.join(' ')
+  end
 end
